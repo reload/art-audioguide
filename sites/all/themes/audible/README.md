acde1b086725251665fc70c56ab02df41320bcea
Camilla Krag Jensen <camilla@reload.dk>
2014-05-12 15:55:23 +0200

    Initial commit.

---
+++ sites/all/themes/audible/README.md
@@ -0,0 +1,16 @@
+# audible theme
+
+# TODO
+This theme was started from the starter theme in Reload\!s start-platform. To start using this theme do these steps:
+
+ * Replace logo.png with ont that makes sense for your theme.
+ * Replace the favicon - unless you are Simon.
+ * Replace the screenshot.png file.
+
+## Set up
+If you don't have Grunt installed, then go follow the [Getting started](http://gruntjs.com/getting-started) guide.
+
+To build the theme, cd to the theme directory and run `npm install`.
+
+## Usage
+To start Grunt, run `grunt` in the theme dir. Now css is compiled to 2 directories: css and css\_dev. The css folder should be checked into git and end up in production and the css\_dev folder is only for local dev use. The difference between the two is that the css\_dev folder is compiled to be readable and with comments, and the css folder is compacted and without comments.
---
+++ sites/all/themes/audible/templates/page.tpl.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @file
+ * Page template for Audible.
+ *
+ * Overridden because we need a lot fewer regions than the default.
+ */
+?>
+<div id="page-wrapper">
+  <div id="page">
+
+    <div class="page-header">
+      <div class="section clearfix">
+
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          </a>
+        <?php endif; ?>
+
+
+        <a href="#menu">&#9776;</a>
+        <?php print render($page['header']); ?>
+
+      </div>
+    </div> <!-- /.section, /#header -->
+
+    <?php print $messages; ?>
+
+    <div id="main-wrapper"><div id="main" class="clearfix">
+
+        <div id="content" class="column"><div class="section">
+            <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+            <a id="main-content"></a>
+            <?php print render($title_prefix); ?>
+            <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+            <?php print render($title_suffix); ?>
+            <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+            <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+            <?php print render($page['content']); ?>
+          </div></div> <!-- /.section, /#content -->
+
+      </div></div> <!-- /#main, /#main-wrapper -->
+
+    <div id="footer"><div class="section">
+        <?php print render($page['footer']); ?>
+      </div></div> <!-- /.section, /#footer -->
+
+  </div>
+</div> <!-- /#page, /#page-wrapper -->
