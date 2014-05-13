38326073ec96d00715191ed7a4bb562a5378b221
Camilla Krag Jensen <camilla@reload.dk>
2014-05-13 14:52:32 +0200

    Style admin tabs super simple.

--- sites/all/themes/audible/templates/page.tpl.php
+++ sites/all/themes/audible/templates/page.tpl.php
@@ -19,6 +19,7 @@
       <?php endif; ?>
 
       <?php print render($page['header']); ?>
+      <?php if ($tabs): ?><div class="admin-tabs"><?php print render($tabs); ?></div><?php endif; ?>
 
     </div> <!-- /.page-header -->
 
@@ -32,7 +33,6 @@
         <?php print render($title_prefix); ?>
         <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
         <?php print render($title_suffix); ?>
-        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
         <?php print render($page['content']); ?>
       </div></div> <!-- /.section, /#content -->
