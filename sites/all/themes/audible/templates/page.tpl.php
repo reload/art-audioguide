87fda1ae0de6244134b3ed20e71110f5c3a2d45c
André Kallehauge <andrekallehauge@gmail.com>
2014-05-13 11:06:18 +0200

    Corrected indentation

--- sites/all/themes/audible/templates/page.tpl.php
+++ sites/all/themes/audible/templates/page.tpl.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @file
  * Page template for Audible.
@@ -7,18 +6,19 @@
  * Overridden because we need a lot fewer regions than the default.
  */
 ?>
+
 <div id="page-wrapper">
   <div id="page">
 
     <div class="page-header">
 
-        <?php if ($logo): ?>
-          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-          </a>
-        <?php endif; ?>
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
 
-        <?php print render($page['header']); ?>
+      <?php print render($page['header']); ?>
 
     </div> <!-- /.page-header -->
 
@@ -26,22 +26,22 @@
 
     <div id="main-wrapper"><div id="main" class="clearfix">
 
-        <div id="content" class="column"><div class="section">
-            <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-            <a id="main-content"></a>
-            <?php print render($title_prefix); ?>
-            <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-            <?php print render($title_suffix); ?>
-            <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-            <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-            <?php print render($page['content']); ?>
-          </div></div> <!-- /.section, /#content -->
+      <div id="content" class="column"><div class="section">
+        <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+        <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+        <?php print render($page['content']); ?>
+      </div></div> <!-- /.section, /#content -->
 
-      </div></div> <!-- /#main, /#main-wrapper -->
+    </div></div> <!-- /#main, /#main-wrapper -->
 
     <div id="footer"><div class="section">
-        <?php print render($page['footer']); ?>
-      </div></div> <!-- /.section, /#footer -->
+      <?php print render($page['footer']); ?>
+    </div></div> <!-- /.section, /#footer -->
 
   </div>
 </div> <!-- /#page, /#page-wrapper -->
