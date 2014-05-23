556db4d634d97842f990c3ddb5ac57c028e260f0
André Kallehauge <andrekallehauge@gmail.com>
2014-05-23 11:23:42 +0200

    #SMKA-83 overwrites the Menu Icons modules default template to fit our hack

---
+++ sites/all/themes/audible/templates/menu_icons_css_item.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ *
+ * Template file for generating the CSS file used for the menu-items
+ */
+
+/**
+ * Variables:
+ * $mlid
+ * $path
+ */
+?>
+.menu-<?php print $mlid; ?> {
+  background-image: url(<?php print $path; ?>);
+}
