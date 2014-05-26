aa11495a65f9f85c4236bb191f432ca5015d7459
André Kallehauge <andrekallehauge@gmail.com>
2014-05-26 10:34:38 +0200

    Enabled resizing of the media-player & slimmed down the options list

--- sites/all/modules/smk_audio/smk-mediaelement-settings.js
+++ sites/all/modules/smk_audio/smk-mediaelement-settings.js
@@ -5,42 +5,35 @@
    */
   Drupal.behaviors.mediaelement = {
     attach: function (context, settings) {
+      // Selectors.
+      var audio = $('audio', context);
 
-      $('audio', context).addClass('mejs-smk');
 
-      $('audio', context).mediaelementplayer({
-        // width of audio player
+      // Add "skin class" to the player.
+      audio.addClass('mejs-smk');
+
+      // Init the player.
+      audio.mediaelementplayer({
         audioWidth: '100%',
-        // height of audio player
-        audioHeight: 30,
-        // initial volume when the player starts
-        startVolume: 1, // default: 0.8.
-        // audio volume direction (horizontal / vertical).
-        audioVolume: 'vertical',
-        // useful for <audio> player loops
-        loop: false,
-        // enables Flash and Silverlight to resize to content size
-        enableAutosize: true,
-        // the order of controls you want on the control bar (and other plugins below)
-        features: ['current', 'playpause','progress','duration'],
-        // Hide controls when playing and mouse is not over the video
-        alwaysShowControls: false,
-        // force iPad's native controls
-        iPadUseNativeControls: false,
-        // force iPhone's native controls
-        iPhoneUseNativeControls: false,
-        // force Android's native controls
-        AndroidUseNativeControls: false,
-        // forces the hour marker (##:00:00)
-        alwaysShowHours: false,
-        // show framecount in timecode (##:00:00:00)
-        showTimecodeFrameCount: false,
-        // turns keyboard support on and off for this instance
-        enableKeyboard: true,
-        // when this player starts, it will pause other players
-        pauseOtherPlayers: true,
-        // array of keyboard commands
-        keyActions: []
+        startVolume: 1,
+        features: ['playpause', 'progress', 'current', 'duration']
+      });
+
+      // To gain control over the width of the mediaelement player, who sets its own
+      // width on pageload, we are instead controlling the width of the player's parent
+      // element. Screwy, I know.
+      function setMediaelementWidth() {
+        if( $(window).width() < 800 ) {
+          $('.mediaelement-audio', context).width( $(window).width() - 24 );
+        }
+      }
+
+      $(document).ready(function($) {
+        setMediaelementWidth();
+      });
+
+      $(window).resize(function(){
+        setMediaelementWidth();
       });
 
     }
