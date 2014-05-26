0036e75f99c858f19579d889e38e1176f34caed8
André Kallehauge <andrekallehauge@gmail.com>
2014-05-26 14:10:35 +0200

    The Number Pad now works with touch/click

---
+++ sites/all/modules/smk_search/numberpad.js
@@ -0,0 +1,116 @@
+(function ($) {
+
+  /**
+   * Number pad.
+   */
+  Drupal.behaviors.numberPadHeight= {
+    attach: function (context, settings) {
+      // Make the keys take up the entire height of the screen
+      function setKeyHeight() {
+        var keyElement = $('.number-pad .key');
+        var viewportHeight = $(window).height() - $('.page-header').outerHeight() - $('.number-pad .display').outerHeight() - $('.number-pad .help').outerHeight();
+
+        // Make sure we have a minimum height
+        if( viewportHeight / 4 > 42 ) {
+          $(keyElement).height(viewportHeight / 4);
+        }
+      }
+
+      // Rezise on page load
+      setKeyHeight();
+
+      // Resize when window width changes
+      $(window).resize(function(){
+        setKeyHeight();
+      });
+    }
+  };
+
+  Drupal.behaviors.numberPadTouch = {
+    attach: function (context, settings) {
+      // Selectors.
+      var page = $('.page-number-search', context);
+      var numberPad = $('.number-pad', page);
+      var form = $('form', page);
+      var input = $('input[name="id"]', form);
+      var display = $('.number-pad div.audio-id-value', page);
+      var clear = $('.clear', numberPad);
+
+      // If there's any results on the page.
+      if ($('.views-row', context).get(0)) {
+        // Hide the numberPad.
+        numberPad.hide();
+      }
+
+      // Add number.
+      function numberPadAddNumber(value) {
+        // Add to hidden input field.
+        input.attr('defaultValue', input.attr('defaultValue') + value );
+
+        // Add to display.
+        display.append(value);
+      }
+
+      // Remove number (one at a time).
+      function numberPadRemoveNumber() {
+        // Remove from hidden input field.
+        input.attr('defaultValue', input.attr('defaultValue').slice(0, -1) );
+
+        // Remove from display.
+        display.text( $().text().slice(0, -1) );
+      }
+
+      // Clear display.
+      function numberPadClear() {
+        // Clear hidden input field.
+        input.attr('defaultValue', '');
+
+        // Clear display.
+        display.text('');
+      }
+
+      // When a number key is clicked.
+      $('.key-number', numberPad).click( function(e){
+        e.preventDefault();
+        var value = $(this).text();
+        numberPadAddNumber(value);
+      });
+
+      // When backspace is clicked.
+      $('.key-backspace', numberPad).click( function(){
+        numberPadRemoveNumber();
+      });
+
+      // When clear is clicked.
+      clear.click( function(){
+        numberPadClear();
+      });
+
+      // Toggle placeholder and clear.
+      $('.key, .clear', numberPad).click( function(e){
+        e.preventDefault();
+
+        if( display.html() ) {
+          // Hide placeholder if display has content.
+          $('.placeholder', numberPad).hide();
+
+          // Show clear button if display has content.
+          clear.show();
+
+        } else {
+          // Add placeholder if display has no content.
+          $('.placeholder', numberPad).show();
+
+          // Hide clear button if display has no content.
+          clear.hide();
+        }
+      });
+
+      // Submit form.
+      $('.key-submit', numberPad).click(function() {
+        form.submit();
+      });
+    }
+  }
+
+})(jQuery);
