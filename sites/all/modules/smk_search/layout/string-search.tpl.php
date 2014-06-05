a439a2f76d859916f2fb80bb12b5b6fa30db3d46
André Kallehauge <andrekallehauge@gmail.com>
2014-06-05 16:49:22 +0200

    Added styling and interactivity for the new search-feature

---
+++ sites/all/modules/smk_search/layout/string-search.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * The template file for the Number-Search markup.
+ */
+?>
+<!-- Search panel -->
+<h2><?php print $headline; ?></h2>
+<div class="help-text"><?php print $help; ?></div>
