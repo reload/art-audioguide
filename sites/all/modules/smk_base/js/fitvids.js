63801f50e6ce46a86f81155b0093d6fc168cbacf
André Kallehauge <andrekallehauge@gmail.com>
2014-06-17 14:11:59 +0200

    Added some scope to "FitVids" library and now shows/hides the foldout-videos box, depending on the videos inside.

--- sites/all/modules/smk_base/js/fitvids.js
+++ sites/all/modules/smk_base/js/fitvids.js
@@ -5,8 +5,14 @@
    */
   Drupal.behaviors.fitVids = {
     attach: function (context, settings) {
-
-      $('.embedded-video', context).fitVids();
+      var video = $('.embedded-video', context);
+      // If there's any embedded videos on the page.
+      if (video.get(0)) {
+        // Init "FitVids" library.
+        video.fitVids();
+        // Display the foldout-videos container.
+        $('.videos', context).show();
+      }
     }
   }
 
