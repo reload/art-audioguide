2b2eb2a6aa4d435f137f39e17583a66e936faffe
André Kallehauge <andrekallehauge@gmail.com>
2014-06-20 12:03:00 +0200

    Added styling to the share buttons

--- sites/all/themes/audible/js/basic.js
+++ sites/all/themes/audible/js/basic.js
@@ -38,6 +38,9 @@
         setTileHeight();
       });
 
+      // Determine if the small tiles are to the left or right.
+      $('.tile--small:even', context).addClass('left');
+      $('.tile--small:odd', context).addClass('right');
     }
   };
 
