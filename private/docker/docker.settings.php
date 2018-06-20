73625043dbf04584e7977950ed468f839f079dba
Michael Søby Andersen <michael@reload.dk>
2018-06-20 12:32:20 +0200

    Added snippet for reverse proxy support

--- private/docker/docker.settings.php
+++ private/docker/docker.settings.php
@@ -1,5 +1,9 @@
 <?php
 
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+  $_SERVER['HTTPS'] = 'on';
+}
+
 $databases = array(
   'default' => array(
     'default' => array(
